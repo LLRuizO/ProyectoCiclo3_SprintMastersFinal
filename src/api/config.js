ef68c1829f1021e97204b7f3f2e1c3b1536add6a
LLRuizO <90738841+LLRuizO@users.noreply.github.com>
2021-11-12 22:58:06 -0500

    cambio url server

--- src/api/config.js
+++ src/api/config.js
@@ -1,2 +1,2 @@
-export const basePath = "http://localhost:3977/api";
+export const basePath = "https://sprintmasters.herokuapp.com//api";
 export const apiVersion = "v1";//URL que nos da nuestro server, lo que se valida cuando iniciamos la carpeta server
