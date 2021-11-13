2074ef6e890ca3dc3d7927c7179e7f8341e35c8e
LLRuizO <90738841+LLRuizO@users.noreply.github.com>
2021-11-12 23:05:15 -0500

    cambios basepath

--- src/api/config.js
+++ src/api/config.js
@@ -1,2 +1,2 @@
-export const basePath = "https://sprintmasters.herokuapp.com//api";
+export const basePath = "https://sprintmasters.herokuapp.com/api";
 export const apiVersion = "v1";//URL que nos da nuestro server, lo que se valida cuando iniciamos la carpeta server
